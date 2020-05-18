2738c0087b0ca3763bb49d7473a5103ad80f9b65
Godeta <Godeta61@gmail.com>
2020-05-18 23:10:18 +0200

    Ajout de lieux + icone interrogation pour les lieux potentiels

--- places/mapView.js
+++ places/mapView.js
@@ -9,6 +9,14 @@ const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'; //s ->tile
 const tiles = L.tileLayer(tileUrl, {
     attribution
 });
+
+//création d'une icône
+const unknown = L.icon({ //icône avec un point d'interrogation 
+    iconUrl: '../img/interrogation.png',
+    iconSize: [25, 25],
+    iconAnchor: [25, 16] //centre de l'image
+});
+
 //liste des marqueurs à ajouter :
 //les tours
 tiles.addTo(mymap);
@@ -41,15 +49,21 @@ tiles.addTo(mymap);
 L.marker([49.281495, -0.224896]).addTo(mymap)
     .bindPopup('Un mini bunker, coord : 49.281495, -0.224896'); //il faut cliquer dessus pour voir le texte
 
-//bunker boite de nuit (pas sûr de où c'est, y'a une maison abandonnée pas loin)
-tiles.addTo(mymap);
-L.marker([49.274464, -0.215778]).addTo(mymap)
-    .bindPopup('Bunker transformé en boite de nuit abandonnée avec une maison abandonnée pas loin (environ par là), coord : 49.274464, -0.215778'); //il faut cliquer dessus pour voir le texte
-
-//bunker ? - bunker boite de nuit ?
+//bunker boîte de nuit
 tiles.addTo(mymap);
 L.marker([49.269451, -0.220943]).addTo(mymap)
-    .bindPopup('Bunker pas sûr lequel, peut être celui de la boite de nuit, coord : 49.269451, -0.220943');
+    .bindPopup('Bunker transformé en boite de nuit abandonnée, coord : 49.269451, -0.220943');
+
+//maison abandonnée pas loin bunker
+//pont abandonné colombelles
+tiles.addTo(mymap);
+L.marker([49.274462, -0.214050]).addTo(mymap)
+    .bindPopup('Maison abandonné proche du bunker boîte de nuit, coord : 49.274462, -0.214050');
+
+//pont abandonné colombelles
+tiles.addTo(mymap);
+L.marker([49.193144, -0.312720]).addTo(mymap)
+    .bindPopup('Pont abandonné, coord : 49.193144, -0.312720'); //il faut cliquer dessus pour voir le texte
 
 //tour de refroidissement
 tiles.addTo(mymap);
@@ -61,7 +75,7 @@ tiles.addTo(mymap);
 L.marker([49.211150, -0.279927]).addTo(mymap)
     .bindPopup('Une petite tour faites de pierres assemblées au milieu du petit bois, coord : 49.211150, -0.279927');
 
-//maison abandonnée
+//maison abandonnée Caen
 tiles.addTo(mymap);
 L.marker([49.179078, -0.340776]).addTo(mymap)
     .bindPopup('<a href = "../Description/maisAB.html" >Maison abandonnée dans le coin vers le début de Caen, il y en a une autre plus loin (cf Nico), coord : 49.179078, -0.340776 </a>'); //il faut cliquer dessus pour voir le texte
@@ -75,3 +89,33 @@ L.marker([49.154766, -0.368643]).addTo(mymap)
 tiles.addTo(mymap);
 L.marker([49.248021, -0.223945]).addTo(mymap)
     .bindPopup('Pas loin de Amfreville, les restes d\' une grange qui a brulée, coord : 49.248021, -0.223945');
+
+//endroits possibles
+
+//pas sûr du tout
+tiles.addTo(mymap);
+L.marker([49.285643, -0.168025], {
+        icon: unknown
+    }).addTo(mymap)
+    .bindPopup('Pas sûr du tout qu\'il y ai un truc, coord : 49.285643, -0.168025');
+
+//y'a moyen 
+tiles.addTo(mymap);
+L.marker([49.285862, -0.160828], {
+        icon: unknown
+    }).addTo(mymap)
+    .bindPopup('Y\'a moyen de trouver un petit coin, coord : 49.285862, -0.160828');
+
+//paumé
+tiles.addTo(mymap);
+L.marker([49.272603, -0.175689], {
+        icon: unknown
+    }).addTo(mymap)
+    .bindPopup('Bâtiment un peu paumé donc peut être ?, coord : 49.272603, -0.175689');
+
+//paumé 2
+tiles.addTo(mymap);
+L.marker([49.270710, -0.183017], {
+        icon: unknown
+    }).addTo(mymap)
+    .bindPopup('Bâtiment un peu paumé donc à voir ?, coord : 49.270710, -0.183017');
